a67751284841a7fa4d8f308efe82256789a25c78
Muktar Usman <mualiyuoox@gmail.com>
2025-02-26 20:59:33 +0100

    U

--- resources/views/layouts/admin/index.blade.php
+++ resources/views/layouts/admin/index.blade.php
@@ -46,11 +46,10 @@
         @yield('content')
 
 
-
         <footer>
           <div class="footer clearfix mb-0 text-muted">
             <div class="float-start">
-              <p>2024 &copy;</p>
+              <p>{{date('Y')}} &copy;</p>
             </div>
           </div>
         </footer>
